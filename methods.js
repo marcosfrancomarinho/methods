6ef7e6172ce5d8df0ae4223be8cfb9c75fdb2ba6
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-01-19 23:46:31 -0300

    19/01/2024 23:46

--- methods.js
+++ methods.js
@@ -60,6 +60,61 @@ class Utility {
                             this._.innerText = content
                         }
                     }
+                },
+                cssCarousel: function (object, elmt) {
+                    const string = Object.keys(object).reduce((united, only) => {
+                        return united + (only + ":" + object[only] + "; ")
+                    }, "")
+                    if (elmt.length > 1) {
+                        for (let e of elmt) {
+                            e.style = string
+                        }
+                    } else {
+                        elmt.style = string
+                    }
+                },
+                carousel: function (obj) {
+                    const container = this._
+                    const carousel = container.children[0]
+                    const links = carousel.children
+                    const width = Number(obj.width.replace("px", ""))
+                    let idx = 0
+                    //div container - que englopa toda a estrutura do carrossel.
+                    this.cssCarousel({
+                        "display": "flex",
+                        "width": obj.width,
+                        "height": obj.height,
+                        "overflow": "hidden",
+                        "border-radius": "3px"
+                    }, container);
+                    //div carousel - div que se movimenta.
+                    this.cssCarousel({
+                        "display": "flex",
+                        "transition": "1s all ease-in-out",
+                    }, carousel);
+                    //elm a - links da imagens que contem a tag img dentro.
+                    this.cssCarousel({
+                        "display": "flex",
+                        "width": obj.width,
+                        "height": obj.height,
+                        "justify-content": "center",
+                        "align-item": "center"
+                    }, links)
+                    //imagens do carrossel
+                    for (let a of links) {
+                        this.cssCarousel({
+                            "width": obj.width,
+                            "height": obj.height
+                        }, a.children[0])
+                    }
+                    //loop do carrossel
+                    setInterval(function () {
+                        if (idx >= links.length) {
+                            idx = 0
+                        }
+                        carousel.style.transform = `translateX(-${width * idx}px)`
+                        idx++
+                    }, obj.time)
                 }
             }
         }
